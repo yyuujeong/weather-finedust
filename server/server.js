64b1c16b9b297b6795d0261b99bca0b4eda23e05
yyuujeong <maple10ten@gmail.com>
2024-03-23 23:11:35 +0900

    server.js

--- server/server.js
+++ server/server.js
@@ -1,31 +1,39 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 
-app.get('/weather/:loaction', async(req, res) => {
+// app.get('/', async(req,res) => {
+//   res.json({ "message": "hi" });
+// })
+
+app.get("/weather/:location", async (req, res) => {
   try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&appid=${process.env.APP_WEATHER_KEY}&units=metric`);
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&appid=${process.env.APP_WEATHER_KEY}&units=metric`
+    );
     res.json(response.data);
   } catch (error) {
-    console.error(error);
+    res.status(500).json({ error: error.message });
   }
 });
 
-app.get('/pollution', async (req, res) => {
+app.get("/pollution", async (req, res) => {
   try {
-      const pollutionRes = await axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${req.query.lat}&lon=${req.query.lon}&appid=${process.env.APP_WEATHER_KEY}`);
-      res.json(pollutionRes.data);
+    const pollutionRes = await axios.get(
+      `https://api.openweathermap.org/data/2.5/air_pollution?lat=${req.query.lat}&lon=${req.query.lon}&appid=${process.env.APP_WEATHER_KEY}`
+    );
+    res.json(pollutionRes.data);
   } catch (error) {
-    console.error(error);
+    res.status(500).json({ error: error.message });
   }
 });
 
 module.exports = app;
 
-app.listen(PORT, () =>console.log(`server is listening on ${PORT}`));
+app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
