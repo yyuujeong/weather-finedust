7636b28feff26f3007a5aa12f8e8bc932ee53bd6
yyuujeong <maple10ten@gmail.com>
2024-07-26 15:50:18 +0900

    webpack, babel

--- README.md
+++ README.md
@@ -6,25 +6,22 @@
 - 2023.12 - 2024.02
 
 ## :hammer_and_pick: 기술
-```React``` ```styled-components``` ```Express``` ```Zustand```
+```React``` ```styled-components``` ```Express``` ```Zustand``` ```Webpack``` ```Babel```
 
 ## :card_index_dividers: 프로젝트 소개
 - 개인 프로젝트
-- 리액트와 Express를 사용했고 Open API를 활용해 보고자 제작
-- Vercel을 통해 배포
+- 리액트와 Express로 제작한 후, Vercel을 통해 배포
 
 ## :mag_right: 주요 기능
-- styled-components로 스타일을 적용해 재사용성을 높임
-- 시간대 별 배경화면 변화
-- react-router를 이용한 페이지 이동 (특정 링크 스타일링을 위해 NavLink 사용)
-- Open API를 통해 RESTful한 API를 사용하여 사용자의 위치와 날씨/대기 정보를 조회
-- 클라이언트 측에서 API 키가 노출되지 않도록 보호하기 위해 Express 백엔드 프레임워크를 사용하여 임시 서버 구축
-- Zustand를 사용해 상태 관리
-- 미디어 쿼리를 사용해 반응형 웹 구현
+- Open API를 통해 RESTful한 방식으로 사용자의 위치와 날씨/대기 정보를 조회
+- Express를 이용해 API 키를 서버 측에서 관리하여 클라이언트에서의 노출을 방지
+- Zustand를 사용한 상태 관리와 웹팩, 바벨을 적용하여 페이지 로딩 시간을 약 3.4초 단축
+- react-router를 통해 페이지 이동을 구현하고, 특정 링크의 스타일링을 위해 NavLink 사용
+- 시간대에 따라 변하는 배경화면을 제공해 사용자의 시각적 경험을 향상
 
 ### :point_right: 날씨 조회
 - 세계 도시를 검색하여 날씨 정보 조회
 ### :point_right: 위치 기반 미세먼지
 - Geolocation API를 이용해 사용자 위치 구함
 - Reverse-Geocoding으로 위도와 경도 값을 주소로 변환
-- 대기 질 상태를 직관적으로 표현하기 위해 이모지와 색상, 메시지를 사용하여 사용자 경험 개선
+- 대기 질 상태를 직관적으로 표현하기 위해 이모지와 색상, 메시지를 사용
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, "client/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          },
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+          NODE_ENV: JSON.stringify('development')
+      }
+  })
+  ],
+devServer: {
+    compress: true,
+    port: 5000,
+  }
+};
